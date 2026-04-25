a0477cf4de1a9cd43e6df7936b8144f9407fda8a
Bryan Silva Botelho <gamezer.to01@gmail.com>
2026-04-24 22:24:08 -0300

    Implementado método DELETE de contatos

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -116,66 +116,96 @@ class ContactService {
     }
 
     async getContacts(query: any) {
-    try {
-        const { page = 1, active = 'true', name } = query;
+        try {
+            const { name, page = 1, active = 'true' } = query;
 
-        const limit = 10;
-        const skip = (Number(page) - 1) * limit;
+            const limit = 10;
+            const skip = (Number(page) - 1) * limit;
 
-        const filters: any = {};
+            const filters: any = {};
 
-        if (active === 'true') {
-            filters.active = true;
-        } else if (active === 'false') {
-            filters.active = false;
-        }
-
-        if (name) {
-            filters.OR = [
-                {
-                    firstName: {
-                        contains: name,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    lastName: {
-                        contains: name,
-                        mode: 'insensitive'
-                    }
-                }
-            ];
-        }
-
-        const contacts = await prisma.contact.findMany({
-            where: filters,
-            skip,
-            take: limit,
-            orderBy: {
-                id: 'asc'
-            },
-            select: {
-                id: true,
-                firstName: true,
-                lastName: true,
-                number: true,
-                active: true,
-                countryId: true
+            if (active === 'true') {
+                filters.active = true;
+            } else if (active === 'false') {
+                filters.active = false;
             }
-        });
 
-        const total = await prisma.contact.count({
-            where: filters
-        });
+            if (name) {
+                filters.OR = [
+                    {
+                        firstName: {
+                            contains: name,
+                            mode: 'insensitive'
+                        }
+                    },
+                    {
+                        lastName: {
+                            contains: name,
+                            mode: 'insensitive'
+                        }
+                    }
+                ];
+            }
 
-        return { status: 200, success: true, contacts, pagination: { total, page: Number(page), limit, totalPages: Math.ceil(total / limit) }
-    };
-    } catch (error) {
-        console.error(error);
-        return { status: 500, success: false, message: getMessage('SERVER_ERROR', this.lang as 'pt')
+            const contacts = await prisma.contact.findMany({
+                where: filters,
+                skip,
+                take: limit,
+                orderBy: {
+                    id: 'asc'
+                },
+                select: {
+                    id: true,
+                    firstName: true,
+                    lastName: true,
+                    number: true,
+                    active: true,
+                    countryId: true
+                }
+            });
+
+            const total = await prisma.contact.count({
+                where: filters
+            });
+
+            return { status: 200, success: true, contacts, pagination: { total, page: Number(page), limit, totalPages: Math.ceil(total / limit) }
         };
+        } catch (error) {
+            console.error(error);
+            return { status: 500, success: false, message: getMessage('SERVER_ERROR', this.lang as 'pt')
+            };
+        }
     }
-}
+
+    async deleteContact(id: number) {
+        try{
+            const validator: Joi.ValidationResult = DeleteContactSchema(this.lang as 'pt')
+            .validate({ id });
+
+            if (validator.error) {
+                const errorMessage = validator.error.details.map(err => err.message).join(', ');
+                return { status: 400, success: false, message: errorMessage };
+            }
+
+            const existingContact = await prisma.contact.findUnique({
+                where: { id }
+            });
+
+            if (!existingContact) {
+                return { status: 404, success: false, message: getMessage('CONTACT_NOT_FOUND', this.lang as 'pt') };
+            }
+
+            await prisma.contact.update({
+                where: { id },
+                data: { active: false }
+            });
+
+            return { status: 200, success: true, message: getMessage('CONTACT_DELETED', this.lang as 'pt') };
+        }catch (error) {
+            console.error(error);
+            return { status: 500, success: false, message: getMessage('SERVER_ERROR', this.lang as 'pt') };
+        }
+    }
 }
 
 export default new ContactService();
